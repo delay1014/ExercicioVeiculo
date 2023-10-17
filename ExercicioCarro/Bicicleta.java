b3d406137b947a96aa3b699f51ef9cab4e21341b
delay1014 <122221340+delay1014@users.noreply.github.com>
2023-10-17 11:24:44 -0300

    Add files via upload

---
+++ ExercicioCarro/Bicicleta.java
@@ -0,0 +1,40 @@
+public class Bicicleta implements Veiculo {
+    private int qtMarchas;
+    private int maxPassageiros;
+    private int marchaAtual;
+
+    public Bicicleta(int qtMarchas, int maxPassageiros) {
+        this.qtMarchas = qtMarchas;
+        this.maxPassageiros = maxPassageiros;
+        this.marchaAtual = 1; // Inicializa a marcha com 1
+    }
+
+    @Override
+    public boolean podeCarregar() {
+        return true;
+    }
+
+    public boolean podeCarregar(int qtPassageiros) {
+        return qtPassageiros <= maxPassageiros;
+    }
+
+    public int getQtMarchas() {
+        return qtMarchas;
+    }
+
+    public void aumentarMarcha() {
+        if (marchaAtual < qtMarchas) {
+            marchaAtual++;
+        }
+    }
+
+    public void diminuirMarcha() {
+        if (marchaAtual > 1) {
+            marchaAtual--;
+        }
+    }
+
+    public int getMarchaAtual() {
+        return marchaAtual;
+    }
+}
